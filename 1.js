f42defc9aff824164b96fce1b83bb22a2f297cb5
MarcosDaSilvaGomes <marcossgoficial@hotmail.com.br>
2022-09-13 11:42:10 -0300

    exercicios

---
+++ 1.js
@@ -0,0 +1,6 @@
+let prompt = require('prompt-sync')();
+let userName= prompt('Type your name: ');
+let weight= prompt('Type your weight: ');
+let height= prompt('Type your height: ');
+let dateOfHospitalized = prompt('Type The date of hospitalized');
+let diabetes = prompt('Do you have diabetes?');
